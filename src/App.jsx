a65cbc5a5393ef08119a4805798aa1f6444ff824
sridhar02 <kattasridhar02@gmail.com>
2023-02-16 19:49:18 +0530

    base-setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-blue-400">
+      
+      <h1 className='text-4xl text-black'>TechSwipe</h1>
+      
+    </div>
+  )
+}
+
+export default App
