885c2857fbf0fd48d3a80327932e814940d8bf93
sridhar02 <kattasridhar02@gmail.com>
2023-02-18 18:20:27 +0530

    WIP

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  <RouterProvider router={router}>
     <App />
-  </>,
+  </RouterProvider>,
 )
