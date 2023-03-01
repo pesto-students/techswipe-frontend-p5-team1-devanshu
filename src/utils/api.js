544744ccd9cd9c31aa72e9f15259c8d9280b9193
sridhar02 <kattasridhar02@gmail.com>
2023-03-01 21:19:03 +0530

    ⚡️ add local storage hook , api & constant utils

---
+++ src/utils/api.js
@@ -0,0 +1,41 @@
+import { axiosClient } from "./axios";
+
+export const getUserInfo = async () => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  const response = await axiosClient.get("/api/user/info", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response?.data?.user;
+};
+
+export const UpdateUserInfo = async () => {
+  const token = JSON.parse(localStorage.getItem("token"));
+  const response = await axiosClient.put("/api/user/info", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  console.log(response);
+
+  return response;
+};
+
+export const getUserMatchedProfiles = async () => {
+  const sampleData = [
+    { name: "hellena", newProfile: true, profilePic: "" },
+    { name: "sully jones", newProfile: false, profilePic: "" },
+    { name: "Aarya stark", newProfile: true, profilePic: "" },
+  ];
+  // const token = JSON.parse(localStorage.getItem("token"));
+  // const response = await axiosClient.put("/api/user/info", {
+  //   headers: {
+  //     Authorization: `Bearer ${token}`,
+  //   },
+  // });
+  // console.log(response);
+
+  return sampleData;
+};
---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+// console.log({ location, userData });
+
+export const options = [
+  { value: "male", label: "Male" },
+  { value: "female", label: "Female" },
+  { value: "others", label: "Others" },
+];
+
+export const developerOptions = [
+  { value: "full stack developer", label: "Full stack Developer" },
+  { value: "frontend Developer", label: "Frontend Developer" },
+  { value: "backend Developer", label: "Backend Developer" },
+  { value: "devops", label: "Devops" },
+  { value: "test engineer", label: "Test engineer" },
+];
