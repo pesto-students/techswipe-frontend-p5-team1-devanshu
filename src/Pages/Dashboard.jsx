0247f4d9a21e15355bf0f01054cdbc47470d1ab9
sridhar02 <kattasridhar02@gmail.com>
2023-03-12 18:00:49 +0530

    ✨ Dashboard profile card changes

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 // import { useNavigate } from "react-router-dom";
-
 import { users } from "../utils/data";
-import { ImageCard } from "../components/ImageCard";
-import { MobileFooter } from "../components/MobileFooter";
+import TinderCard from "react-tinder-card";
+import { useMutation } from "@tanstack/react-query";
+//
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
-import TinderCard from "react-tinder-card";
+import { ImageCard } from "../components/ImageCard";
+import { MobileFooter } from "../components/MobileFooter";
+import { postUserDisLike, postUserLike } from "../utils/api";
+import DisLike from "../assets/dislike.svg";
+import Like from "../assets/like.svg";
 
 export const Dashboard = () => {
+  const handleLikeMutation = useMutation({
+    mutationFn: postUserLike,
+    onSuccess: () => {
+      // navigate("/dashboard");
+    },
+  });
+
+  const handleDislikeMutation = useMutation({
+    mutationFn: postUserDisLike,
+    onSuccess: () => {
+      // navigate("/dashboard");
+    },
+  });
   const onSwipe = (direction) => {
     console.log("You swiped: " + direction);
   };
@@ -25,19 +42,36 @@ export const Dashboard = () => {
           <Sidebar />
         </div>
       </div>
-      <div className="flex flex-col items-center w-full h-full overflow-scroll relative">
-        {users.map((character, index) => (
-          <TinderCard
-            className="absolute"
-            key={index}
-            onSwipe={onSwipe}
-            onCardLeftScreen={() => onCardLeftScreen("fooBar")}
-            preventSwipe={["right", "left"]}
-          >
-            <ImageCard character={character} key={index} />
-          </TinderCard>
-        ))}
+      <div className="flex flex-col items-center w-full h-full overflow-scroll">
+        <div className="flex flex-col items-center w-full relative">
+          {users.map((character, index) => (
+            <TinderCard
+              className="absolute"
+              key={index}
+              onSwipe={onSwipe}
+              onCardLeftScreen={() => onCardLeftScreen("fooBar")}
+              preventSwipe={["right", "left"]}
+            >
+              <ImageCard character={character} key={index} />
+            </TinderCard>
+          ))}
+        </div>
+        <div className="border-0 flex w-80 mt-[530px] justify-between">
+          <img
+            src={DisLike}
+            alt=""
+            className="h-20 w-20"
+            onClick={handleDislikeMutation}
+          />
+          <img
+            src={Like}
+            className="h-20 w-20"
+            alt=""
+            onClick={handleLikeMutation}
+          />
+        </div>
       </div>
+
       <MobileFooter />
     </div>
   );
